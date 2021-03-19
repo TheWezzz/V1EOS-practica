a62dd1cbdaeb069bed1798480cba81f0d62d0358
TheWezzz <wesley.vanvliet@student.hu.nl>
2021-03-19 18:00:01 +0100

    Laatste versie shell

--- os_level/shell.cc
+++ os_level/shell.cc
@@ -2,9 +2,12 @@
 
 int main()
 { std::string input;
+  std::string prompt = "";
 
-  // ToDo: Vervang onderstaande regel: Laad prompt uit bestand
-  std::string prompt = "ToDo: Laad de prompt uit een bestand! ";
+{ int fd = syscall(SYS_open, "prompt", O_RDONLY, 0755);
+  char byte[1];
+  while(syscall(SYS_read, fd, byte, 1))
+    prompt.append(byte); }
 
   while(true)
   { std::cout << prompt;                   // Print het prompt
@@ -20,14 +23,71 @@ int main()
 
     if (std::cin.eof()) return 0; } }      // EOF is een exit
 
+
 void new_file() // ToDo: Implementeer volgens specificatie.
-{ std::cout << "ToDo: Implementeer hier new_file()" << std::endl; }
+{ std::string filename = "";
+  std::string line = "";
+  std::string text = "";
+
+  std::cout << "Geef de bestandsnaam: " << std::endl;
+  std::getline(std::cin, filename);
+  
+  std::cout << "Geef de tekst voor in " << filename << std::endl;
+  while(std::getline(std::cin, line))
+  { if(line == "<EOF>") break;
+    else {text.append(line + "\n");} }
+  
+  const char* file = filename.c_str();
+  const char* ctext = text.c_str();
+
+  int fd = syscall(SYS_creat, file, 0755);
+  //char byte(text.size() = {text};
+  syscall(SYS_write, fd, ctext, text.size()); }
+
 
 void list() // ToDo: Implementeer volgens specificatie.
-{ std::cout << "ToDo: Implementeer hier list()" << std::endl; }
+{ int child = syscall(SYS_fork);
+  if(child > 0){
+    syscall(SYS_wait4, child, NULL, NULL, NULL);
+  }else if(child == 0){
+    const char* argumenten[4] = {"/bin/ls", "-l", "-a", NULL};
+    syscall(SYS_execve, argumenten[0], argumenten, NULL); } }
+
 
 void find() // ToDo: Implementeer volgens specificatie.
-{ std::cout << "ToDo: Implementeer hier find()" << std::endl; }
+{ int fd[2];
+  syscall(SYS_pipe, &fd);
+  
+  int child = syscall(SYS_fork);
+  if(child > 0){
+    int child2 = syscall(SYS_fork);
+    
+    if(child2 > 0){
+      syscall(SYS_close, fd[0]);
+    syscall(SYS_close, fd[1]);
+      syscall(SYS_wait4, child2, NULL, NULL, NULL);
+    }else if (child2 == 0){
+
+      std::cout << "Geef een string: ";
+      std::string input = "";
+      std::getline(std::cin, input);
+      const char* cinput = input.c_str();
+
+      syscall(SYS_close, fd[1]);
+      syscall(SYS_dup2, fd[0], 0);
+
+      const char* argumenten[3] = {"/bin/grep", cinput, NULL};
+      syscall(SYS_execve, argumenten[0], argumenten, NULL);
+      }
+    
+    syscall(SYS_wait4, child, NULL, NULL, NULL);
+
+  }else if(child == 0){
+    syscall(SYS_close, fd[0]);
+    syscall(SYS_dup2, fd[1], 1);
+
+    const char* argumenten[3] = {"/usr/bin/find", ".", NULL};
+    syscall(SYS_execve, argumenten[0], argumenten, NULL); } }
 
 void seek() // ToDo: Implementeer volgens specificatie.
 { std::cout << "ToDo: Implementeer hier seek()" << std::endl; }
