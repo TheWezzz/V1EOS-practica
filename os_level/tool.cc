bfc825a6d03e5abc1aa5e7d53b32d2155064d51a
TheWezzz <wesley.vanvliet@student.hu.nl>
2021-03-19 14:38:51 +0100

    Laatste versie tool.cc

--- os_level/tool.cc
+++ os_level/tool.cc
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <string>
 
-std::string translate(std::string line, std::string argument)
-{ std::string result = ""; // implementeer dit
+std::string translate(std::string line, std::string append_line)
+{ std::string result = line; // implementeer dit
+  result.append(" ");
+  result.append(append_line);
   return result; }
 
 int main(int argc, char *argv[])
-{ std::string line;
+{ std::string input;
 
   if(argc != 2)
   { std::cerr << "Deze functie heeft exact 1 argument nodig" << std::endl;
     return -1; }
 
-  while(std::getline(std::cin, line))
-  { std::cout << translate(line, argv[1]) << std::endl; } 
-
+  while(true){
+    std::getline(std::cin, input);
+    if(input == "EOF"){
+      break;
+    }
+  std::cout << translate(input, argv[1]) << std::endl;}
   return 0; }
